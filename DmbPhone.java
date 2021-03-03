2e3029416f388b64904a4158da913bc502fe1a43
chocolate90 <74293804+chocolate90@users.noreply.github.com>
2021-03-03 20:25:33 +0900

    상속 핸드폰 Quiz

---
+++ DmbPhone.java
@@ -0,0 +1,29 @@
+package quiz04;
+
+public class DmbPhone extends Phone  {
+	//부모클래스는 수정하지 않습니다.
+	//DmbPhone은 Phone클래스를 상속 받습니다.
+	//channel 변수를 생성하고, color, model, channel을 초기화 하는 생성자를 작성하세요.
+	//메인메서드가 정상적으로 실행되도록 메서드를 추가하세요.
+	
+	int channel;
+	
+	DmbPhone(String m, String c, int ch) {
+		model = m;
+		color = c;
+		channel = ch;
+	}
+	
+	void turnOnDmb() {
+		System.out.println("TV를 켭니다.");
+	}
+
+	void changeChannel(int a) {
+		channel = a;
+		System.out.println("체널을 " + a + "번으로 바꿉니다.");
+	}
+	void turnOffDmb() {
+		System.out.println("TV를 끕니다.");
+	}
+	
+}
---
+++ MainClass_Phone.java
@@ -0,0 +1,41 @@
+﻿package quiz04;
+
+public class MainClass_Phone {
+
+	public static void main(String[] args) {
+		
+		//Dmb폰 객체 생성
+		DmbPhone dp = new DmbPhone("햅틱", "메탈그레이", 10);
+
+		System.out.println("모델:" + dp.model + ", 색상:" + dp.color + ", 체널:" + dp.channel);
+		
+		System.out.println("-------------------------");
+		//Phone클래스에서 상속받은 멤버 변수에 접근
+
+		
+		dp.model = "연아의햅틱";
+		dp.color = "화이트";
+
+		System.out.println("모델:" + dp.model + ", 색상:" + dp.color);
+		
+		System.out.println("--------------------------");
+		//Phone에서 상속받은 메서드 호출
+
+		dp.powerOn();
+		dp.ring();
+		dp.sendVoice("안녕");
+		dp.recieveVoice("그래 안녕");
+		dp.hangUp();
+		dp.powerOff();
+
+		System.out.println("--------------------------");
+		//DmbPhone에서 선언한 메서드 호출
+		dp.turnOnDmb(); //기능: TV를 켭니다 출력
+		dp.changeChannel(7); //기능: channel변수를 전달받은 변수로 변경후 출력
+		dp.turnOffDmb(); //기능: TV를 끕니다 출력
+		dp.powerOff();
+
+
+	}
+
+}
